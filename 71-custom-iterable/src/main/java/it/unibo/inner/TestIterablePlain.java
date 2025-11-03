f3aa50f493b6d869e336254ae1b66374873fecdb
Chiara <chiara.emina@studio.unibo.it>
2025-11-03 14:10:24 +0100

    prepared TestIterablePlain and IterableImpl for run a test

--- 71-custom-iterable/src/main/java/it/unibo/inner/TestIterablePlain.java
+++ 71-custom-iterable/src/main/java/it/unibo/inner/TestIterablePlain.java
@@ -6,12 +6,14 @@ import java.util.List;
 
 import static it.unibo.inner.test.Assertions.assertContentEqualsInOrder;
 
+import it.unibo.inner.impl.IterableImpl;
+
 public class TestIterablePlain {
 
     private TestIterablePlain() {}
 
     private static <T> IterableWithPolicy<T> getIterableWithPolicy(T[] elements) {
-        return null; // TODO: return the implementation of IterableWithPolicy
+        return new IterableImpl(elements); 
     }
 
     public static void main(final String[] args) {
--- 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableImpl.java
+++ 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableImpl.java
@@ -1,16 +1,50 @@
 package it.unibo.inner.impl;
 
-import it.unibo.inner.api;
+import it.unibo.inner.api.IterableWithPolicy;
+import it.unibo.inner.api.Predicate;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class IterableImpl<T> implements IterableWithPolicy<T>{
     
-    private ArrayList<T> list;
+    private List<T> list;
+    private int i;
 
-    IterableImpl<T>(final <T> elements[]){
-       this.list = new ArrayList<T>(elements);
+    public IterableImpl(final T[] elements){
+       this.list = new ArrayList<>();
+       for(T i : elements){
+            list.add(i);
+       }
     }
 
-    void setIteartionPolicy(Predicate<T> filter){
+    public void setIterationPolicy(Predicate<T> filter){
+    }
 
+    public InnerIterator iterator(){
+        return new InnerIterator();
+    }
+
+    class InnerIterator implements Iterator<T>{
+        private int i;
+
+        public InnerIterator(){
+            this.i = -1;
+        }
+
+        public boolean hasNext(){
+            if(i + 1 < IterableImpl.this.list.size()){
+                return true;
+            }
+            return false;
+        }
+
+        public T next(){
+            if(hasNext()){
+                i++;
+                return IterableImpl.this.list.get(i);
+            }
+            return null;
+        }
     }
 }
