95e67595ea7b1ef03e80352ac581b313a1bdbe2c
Chiara <chiara.emina@studio.unibo.it>
2025-11-05 23:15:50 +0100

    create enum Month

--- 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -1,6 +1,8 @@
 package it.unibo.nestedenum;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
 
@@ -8,6 +10,36 @@ import java.util.Objects;
  * Implementation of {@link MonthSorter}.
  */
 public final class MonthSorterNested implements MonthSorter {
+
+    private static int beginIndex = 0;
+    private static int endIndex = 1;
+
+    private enum Month{
+        JANUARY(31),
+        FEBRUARY(28),
+        MARCH(31),
+        APRIL(30),
+        MAY(31),
+        JUNE(30),
+        JULY(31),
+        AUGUST(31),
+        SEPTEMBER(30),
+        OCTOBER(31),
+        NOVEMBER(30),
+        DECEMBER(31);
+
+        private int days;
+
+        private Month(final int days){
+            this.days = days;
+        }
+
+        public int getDays(){
+            return this.days;
+        }
+
+        
+    }
 
     @Override
     public Comparator<String> sortByDays() {
