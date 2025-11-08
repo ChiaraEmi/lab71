e1c0d3e6fc2102e80443f4aed38b8b1720cc2e98
Chiara <chiara.emina@studio.unibo.it>
2025-11-08 16:24:31 +0100

    implemented setUp()

--- 73-junit/src/test/java/it/unibo/bank/impl/TestStrictBankAccount.java
+++ 73-junit/src/test/java/it/unibo/bank/impl/TestStrictBankAccount.java
@@ -21,7 +21,8 @@ class TestStrictBankAccount {
      */
     @BeforeEach
     public void setUp() {
-        fail("To be implemented");
+        this.mRossi = new AccountHolder("Mario", "Rossi", 1);
+        this.bankAccount = new SimpleBankAccount(mRossi, 0.0);
     }
 
     /**
